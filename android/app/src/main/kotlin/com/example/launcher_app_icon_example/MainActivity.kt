bc64daefa7cf383748af1d7c9ff84a7943cb2220
JohannesMilke <johannesmilke1995@gmail.com>
2021-01-26 19:02:54 +0000

    init

---
+++ android/app/src/main/kotlin/com/example/launcher_app_icon_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.launcher_app_icon_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Launcher App Icons';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.red),
+        home: MainPage(title: title),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  final String title;
+
+  const MainPage({
+    @required this.title,
+  });
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              buildImage('assets/icon.png'),
+              const SizedBox(height: 32),
+              buildImage('assets/icon_android.png'),
+              const SizedBox(height: 32),
+              buildImage('assets/icon_ios.png'),
+            ],
+          ),
+        ),
+      );
+
+  Widget buildImage(String path) => Container(
+        width: 160,
+        height: 160,
+        child: Image.asset(path),
+      );
+}
